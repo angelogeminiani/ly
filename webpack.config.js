3e2920294431149726f5cc95ef6c6d6ece83d17d
gian angelo geminiani <angelo.geminiani@gmail.com>
2017-11-09 11:38:33 +0100

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -25,4 +25,29 @@ const config = {
     }
 };
 
-module.exports = config;
+const config_sample = {
+
+    entry: './src_sample/index.ts',
+
+    output: {
+        path: path.resolve(__dirname, 'build_sample'),
+        filename: 'bundle.js'
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
+
+module.exports = [config, config_sample];
