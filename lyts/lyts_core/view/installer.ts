fd62c7708d9899a36a1a83ca23974c7c8486e382
gian angelo geminiani <angelo.geminiani@gmail.com>
2018-07-04 15:54:32 +0200

    no message

--- lyts/lyts_core/view/installer.ts
+++ lyts/lyts_core/view/installer.ts
@@ -3,6 +3,38 @@
  *  MANAGE APP DESKTOP INSTALLATION:
  *  https://developers.google.com/web/fundamentals/app-install-banners/
  *
+ *  To install your web app you need a Service Worker
+ *  <script>
+ *
+ *  var cache_files = [...]; // array of paths with files to add to cache (js, css, images, etc..)
+ *  // Installing Service Worker
+ self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(cache_files);
+        })
+    );
+});
+
+ // Fetching content using Service Worker
+ self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+ *  </script>
+ *
  */
 import console from "../commons/console";
 
